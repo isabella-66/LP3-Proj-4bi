bf3c9eb7eb9b75fb16d405cbd7ee60fbff0f051d
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-11-29 23:03:31 -0300

    feat: add asp validation in create product view

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,33 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace LP3_ProjetoFinal.Models;
 
 public class Product
 {
+    [Required]
     public int Id { get; set; }
+
+    [StringLength(20)]
+    [Required]
     public string Name { get; set; }
+
+    [Required]
+    [Range(0.1, 9999)]
     public float Price { get; set; }
+
+    [Required]
+    [StringLength(200)]
     public string Description { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
     public int Quantity { get; set; }
+
     [ForeignKey("Store")]
+    [Required]
     public int StoreId { get; set; }
+    
     public Store Store { get; set; }
 
     public Product() { }
