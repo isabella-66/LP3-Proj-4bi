2fc998d89d6e294733d326a14b8b81b3e38dc222
isabella-66 <amasprog@gmail.com>
2022-11-30 02:21:37 -0300

    feat: add attributes film

--- Models/Film.cs
+++ Models/Film.cs
@@ -8,21 +8,25 @@ public class Film
     public string Title { get; set; }
     public string Director { get; set; }
     public string Description { get; set; }
-    // public string Duration { get; set; }
     public int Ticket { get; set; }
-    // public string Session { get; set; }
+    public int DaySession { get; set; }
+    public string MonthSession { get; set; }
+    public int HourSession { get; set; }
     // [ForeignKey("MovieTheater")]
     public int MovieTheater { get; set; }
     
     public Film() { }
 
-    public Film(int id, string title, string director, string description, int ticket, int movieTheater)
+    public Film(int id, string title, string director, string description, int ticket, int daySession, string monthSession, int hourSession, int movieTheater)
     {
         Id = id;
         Title = title;
         Director = director;
         Description = description;
         Ticket = ticket;
+        DaySession = daySession;
+        MonthSession = monthSession;
+        HourSession = hourSession;
         MovieTheater = movieTheater;
     }    
 }
