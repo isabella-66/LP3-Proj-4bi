2137c24adb7979cb45461a85dea5c44dfc33ef08
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-11-29 22:37:22 -0300

    feat: add validation in store create view

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LP3_ProjetoFinal.Models;
 
 public class Store
 {
+    [Required]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
+
+    [Required]
+    [StringLength(200)]
     public string Description { get; set; }
     
     public Store() { }
